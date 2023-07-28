16d0147d997a360ad72e536d040440770f83a923
CloudLearner071923 <140013714+CloudLearner071923@users.noreply.github.com>
2023-07-28 12:28:27 -0500

    Update second_streamlit_app.py
    initial setup

--- second_streamlit_app.py
+++ second_streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+#import streamlit
+streamlit.title('My Parents New Healthy Diner')
 
